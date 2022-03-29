8ad19bb1cfb7de32043eeb7ec6f77967d38f0148
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-03-29 15:41:05 -0400

    Split-Screen Design Pattern

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {SplitScreen} from "./SplitScreen";
 
-const LeftHandComponent = () => {
-  return <h1 style={{backgroundColor:'#9c4952'}}>Left</h1>
+const LeftHandComponent = ({name}) => {
+  return <h1 style={{backgroundColor:'#9c4952', color:'white'}}>{name}</h1>
 }
 
-const RightHandComponent = () => {
-  return <h1 style={{backgroundColor:'#f8986d'}}>Right</h1>
+const RightHandComponent = ({message}) => {
+  return <h1 style={{backgroundColor:'#f8986d', color:'white'}}>{message}</h1>
 }
 
 function App() {
   return (
-    <SplitScreen left={LeftHandComponent} right={RightHandComponent}></SplitScreen>
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Gill"></LeftHandComponent>
+      <RightHandComponent message="hello"></RightHandComponent>
+    </SplitScreen>
   );
 }
 
