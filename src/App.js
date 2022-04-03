e10aca30660aff82d70f26d4d24ff9b7279477aa
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-02 20:11:13 -0400

    UserInfo.js created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { UserInfo } from "./UserInfo";
 
 function App() {
-    return <div className='app'>
-         
-    </div>;
+  return <div className="app">
+      <CurrentUserLoader>
+          
+      </CurrentUserLoader>
+  </div>;
 }
 
 export default App;
