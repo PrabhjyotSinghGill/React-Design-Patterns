0b50d6afb9de410a4a47dea3102172749e4a17ea
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-03 13:49:41 -0400

    DataSource component using async function

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from "react";
 import { UserInfo } from "./UserInfo";
 import { UserLoader } from "./UserLoader";
 import { ResourceLoader } from "./ResourceLoader";
-import {ProductInfo} from "./ProductInfo";
-import {DataSource} from "./DataSource";
+import { ProductInfo } from "./ProductInfo";
+import { DataSource } from "./DataSource";
+import axios from "axios";
+
+const getServerData = url => async () =>{
+  const response = await axios.get(url);
+  return response.data;  
+}
 
 
 function App() {
   return (
     <>
-    <ResourceLoader resourceUrl="/users/1" resourceName="user">
-      <UserInfo></UserInfo>
-    </ResourceLoader>
-    <ResourceLoader resourceUrl="/products/001" resourceName="product">
-      <ProductInfo></ProductInfo>
-    </ResourceLoader>
+      <DataSource
+        getDataFunc={getServerData('/users/1')}
+        resourceName="user"
+      >
+        <UserInfo></UserInfo>
+      </DataSource>
     </>
   );
 }
