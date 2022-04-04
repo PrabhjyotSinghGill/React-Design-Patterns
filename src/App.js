8ec86a4024c34a3aa862684f2f5967472edbe2a8
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-04-04 16:28:31 -0400

    Collecting onboarding data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 
-const StepOne = ({goToNext}) => (<><h1>Step 1</h1> <button onClick={goToNext}>Next</button></>);
-const StepTwo = ({goToNext}) =>  (<><h1>Step 2</h1> <button onClick={goToNext}>Next</button></>);
-const StepThree = ({goToNext}) =>  (<><h1>Step 3</h1> <button onClick={goToNext}>Next</button></>);
+const StepOne = ({goToNext}) => (<><h1>Step 1</h1> <button onClick={() => goToNext({name:'John Doe'})}>Next</button></>);
+const StepTwo = ({goToNext}) =>  (<><h1>Step 2</h1> <button onClick={() => goToNext({age:100})}>Next</button></>);
+const StepThree = ({goToNext}) =>  (<><h1>Step 3</h1> <button onClick={() => goToNext({hairColor:'brown'})}>Next</button></>);
 
 function App() {
     const [shouldShowModal, setShouldShowModal] = useState(false);
     
     return <>
-        <UncontrolledOnboardingFlow>
+        <UncontrolledOnboardingFlow onFinish={data =>{console.log(data); alert('Onboarding complete!');}}>
             <StepOne></StepOne>
             <StepTwo></StepTwo>
             <StepThree></StepThree>
