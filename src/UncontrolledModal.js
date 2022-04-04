b8e9163867eb47876db87f47645fa9a008a96956
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-04-04 14:00:57 -0400

    Uncontrolled Modal

---
+++ src/UncontrolledModal.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+const ModalBackground = styled.div`
+    position:fixed;
+    z-index:1;
+    left:0;
+    top:0;
+    width:100%;
+    height:100%;
+    overflow:auto;
+    background-color:rgba(0,0,0,0.5);
+`;
+
+const ModalBody = styled.div`
+    background-color:white;
+    margin:10% auto;
+    padding:20px;
+    width:50%;
+`;
+
+export const UncontrolledModal = ({children}) =>{
+    const [shouldShow, setShouldShow] = useState(false);
+    
+    return(
+        <>
+            <Button onClick={() => setShouldShow(true)}>Show Modal</Button>
+            {shouldShow && (
+
+                <ModalBackground onClick={() => setShouldShow(false)}>
+                    <ModalBody onClick={e => e.stopPropagation()}>
+                        <button onClick={() => setShouldShow(false)}>Hide Modal</button>
+                        {children}
+                    </ModalBody>
+                </ModalBackground>
+            )}
+        </>
+    )
+}
