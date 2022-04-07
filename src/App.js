e8da8b7352668e19c398b36097ffa4a6c2834719
Prabhjyot Singh <prabhjyotg@gmail.com>
2022-04-06 23:15:00 -0400

    ControlledOnboardingFlow modified1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import React, { useState} from 'react';
+import {ControlledOnboardingFlow} from './ControlledOnboardingFlow';
 
 const StepOne = ({goToNext}) => (<><h1>Step 1</h1> <button onClick={() => goToNext({name:'John Doe'})}>Next</button></>);
 const StepTwo = ({goToNext}) =>  (<><h1>Step 2</h1> <button onClick={() => goToNext({age:100})}>Next</button></>);
 const StepThree = ({goToNext}) =>  (<><h1>Step 3</h1> <button onClick={() => goToNext({hairColor:'brown'})}>Next</button></>);
 
 function App() {
+    const [onboardingData, setOnboardingData] = useState({});
+    const [currentIndex, setCurrentIndex] = useState(0);
     const [shouldShowModal, setShouldShowModal] = useState(false);
     
     const onNext = stepData => {
@@ -17,7 +19,7 @@ function App() {
         <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
             <StepOne></StepOne>
             <StepTwo></StepTwo>
-            <StepThree></StepThree>
+            {onboardingData.age >= 62 && <StepThree></StepThree>}
         </ControlledOnboardingFlow>
     </>;
 }
